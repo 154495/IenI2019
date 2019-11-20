acbafa0439f340d6adf8b9e6e8653b0ce1f19584
154495 <154495@csgaugustinus.nl>
2019-11-20 09:50:16 +0000

    s

--- JS/H1/H1O03.js
+++ JS/H1/H1O03.js
@@ -9,10 +9,16 @@ function setup() {
 function draw() {
   noStroke();
   fill(0,0,255,0.3);
-  fill('red');
+   // fill('red');
   rect(0,150,450,150);
-  fill('green');
+   // fill(0,255,0);
   rect(0,0,150,450);
-  fill('blue');  
+   // fill(0,0,255);  
   rect(75,0,375,450);
+
+  noStroke();
+  ellipse(450,225,450);
+
+  noStroke();
+  triangle(225,0,450,225,225,450)
 }
