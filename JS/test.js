3de614b9af36c89842ced9fcd5df9378f30fed43
154495 <154495@csgaugustinus.nl>
2019-12-05 08:41:23 +0000

    opdrachten

--- JS/test.js
+++ JS/test.js
@@ -6,9 +6,7 @@ function setup() {
 }
 
 function draw() {
-  noStroke();
+  stroke('black');
   fill('steelblue');
-  ellipse(0,0,800);
-  fill('deepskyblue');
-  ellipse(450,450,400);
+  ellipse(225,225,300);
 }
